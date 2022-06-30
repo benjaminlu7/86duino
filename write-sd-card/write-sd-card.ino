9141a7267dd4cd8f782b716ddc4e914b041f02c3
Benjamin Lu <77364151+benjaminlu7@users.noreply.github.com>
2022-06-30 10:25:00 -0700

    initial commit

---
+++ write-sd-card/write-sd-card.ino
@@ -0,0 +1,70 @@
+/*
+================================================================================================
+Write SD Card
+================================================================================================
+A simple writable script using 86Duino Zero or 86Duino Educake to display the outcome to Serial 
+Monitor.
+  
+@package   Write SD Card
+@author    ICOP Technology Inc.
+@copyright Copyright 2022. ICOP Technology Inc.
+@license   GNU General Public License v2 or later
+@link      https://www.gnu.org/licenses/gpl-2.0.html
+================================================================================================
+*/
+
+/*
+================================================================================================
+Table of Content
+================================================================================================
+ 1.0 - Required Files
+ 2.0 - Setup
+ 3.0 - Loop
+================================================================================================
+*/
+
+/*
+================================================================================================
+ 1.0 - Required Files
+================================================================================================
+*/
+#include <SD.h>
+
+File configFile;
+
+/*
+================================================================================================
+ 2.0 - Setup
+================================================================================================
+*/
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin( 9600 );
+
+  while( ! Serial ) {
+    // Wait for serial port to connect. Needed for 86Duino Zero or 86Duino Educake only.  
+  }
+
+  configFile = SD.open( "config.txt", FILE_WRITE );
+
+    // if the file opened okay, write to it:
+  if ( configFile ) {
+    configFile.println("name=Benjamin Lu");
+  // close the file:
+    configFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+/*
+================================================================================================
+ 3.0 - Loop
+================================================================================================
+*/
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
